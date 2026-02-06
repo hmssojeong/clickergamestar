709dd4faffacf44b6305ede33a2dec7c067595f8
hmssojeong <sojeong9638@gmail.com>
2026-02-06 16:42:41 +0900

    chore: 폴더 위치수정

--- Assets/01.Scripts/Tree/TreeOutgame/Feature/02.Repository/Upgrade/IUpgradeRepository.cs
+++ Assets/01.Scripts/Tree/TreeOutgame/Feature/02.Repository/Upgrade/IUpgradeRepository.cs
@@ -1,18 +1,8 @@
 using Cysharp.Threading.Tasks;
 
-/// <summary>
-/// 업그레이드 데이터 저장소 인터페이스
-/// Local과 Firebase 구현체가 이 인터페이스를 따릅니다
-/// </summary>
 public interface IUpgradeRepository
 {
-    /// <summary>
-    /// 업그레이드 데이터를 저장합니다
-    /// </summary>
     UniTaskVoid Save(UpgradeSaveData data);
 
-    /// <summary>
-    /// 업그레이드 데이터를 불러옵니다
-    /// </summary>
     UniTask<UpgradeSaveData> Load();
 }
