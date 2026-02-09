4422253ce04ba023b1e91382eb044c834d533489
hmssojeong <sojeong9638@gmail.com>
2026-02-09 16:03:44 +0900

    chore: 비밀번호 암호화 빼기

--- Assets/01.Scripts/Tree/TreeOutgame/Feature/02.Repository/Account/LocalAccountRepository.cs
+++ Assets/01.Scripts/Tree/TreeOutgame/Feature/02.Repository/Account/LocalAccountRepository.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LocalAccountRepository : IAccountRepository
 {
-    private const string SALT = "kk23";
+/*    private const string SALT = "kk23";*/
 
     public bool IsEmailAvailable(string email)
     {
@@ -29,17 +29,17 @@ public class LocalAccountRepository : IAccountRepository
             });
         }
 
-        string hashedPassword = Crypto.HashPassword(password, SALT);
+/*        string hashedPassword = Crypto.HashPassword(password, SALT);*/
         // 해싱문자열: 문자열을 특정 알고리즘을 이용해서 변경된 고정된 길이의 문자열 
         // 성훈씨 아이디 : tjdgnd1004
         //      비밀번호: 10041004!
 
-        PlayerPrefs.SetString(email, hashedPassword);
+        PlayerPrefs.SetString(email, password);
 
         return new UniTask<AccountResult>(new AccountResult
         {
             Success = true,
-            Account = new Account(email, hashedPassword),
+            Account = new Account(email, password),
         });
     }
 
@@ -57,7 +57,7 @@ public class LocalAccountRepository : IAccountRepository
 
         // 3. 비밀번호 틀렸다면 실패.
         string myPassword = PlayerPrefs.GetString(email);
-        if (Crypto.VerifyPassword(password, myPassword, SALT))
+        if (myPassword != password)
         {
             return new UniTask<AccountResult>(new AccountResult
             {
